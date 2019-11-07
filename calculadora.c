e4defcdfc1194ff54266acfa77b26a0df60d5da8
Faria <a18598@alunos.ipca.pt>
2019-11-07 16:02:48 +0000

    Codigo da divisao

--- calculadora.c
+++ calculadora.c
@@ -11,16 +11,6 @@ void soma()
 
 void subtrai()
 {
-    float num1;
-        float num2;
-        float resultado;
-
-        printf("Escolha o primeiro numero:");
-        scanf("%f", &num1);
-        printf("Escolha o segundo numero:");
-        scanf("%f", &num2);
-        resultado = num1 - num2;
-        printf("resultado = %f", resultado);
 
     system ("pause");
     system("cls"); 
@@ -28,8 +18,18 @@ void subtrai()
 }
 
 void divide()
-{ 
+{
+    float num1;
+    float num2;
+    float resultado;
 
+    printf("Escolha o primeiro numero:");
+    scanf("%f", &num1);
+    printf("Escolha o segundo numero:");
+    scanf("%f", &num2);
+    resultado = num1 / num2;
+    printf("resultado = %f", resultado);
+    
     system ("pause");
     system("cls");
     menu();
@@ -37,7 +37,7 @@ void divide()
 
 void multiplica()
 { 
-    
+
     system ("pause");
     system("cls");
     menu();
